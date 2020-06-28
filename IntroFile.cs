4e8f48852b39705d2f75d0afe9278a88a203b7c4
WilliamAughenbaugh <williamrulesftw224@gmail.com>
2020-06-27 19:06:10 -0600

    Update and rename IntroFile to IntroFile.cs

---
+++ IntroFile.cs
@@ -0,0 +1,69 @@
+ private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+
+        static void Main(string[] args) => new Program().RunChamsyBot().GetAwaiter().GetResult();
+
+        public async Task RunChamsyBot()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+
+            //event supscription, Ties Bot to DiscordApp API
+            _client.Log += Log;
+
+            string botToken = "NTM0MjU0NjUxMTg1NTYxNjAx.Dx3Ltw.M6oxBs9Bmr88hBNPwmURElpmszI";
+
+            await RegisterCommandsAsync();
+
+            // Logs Bot Online.
+            await _client.LoginAsync(TokenType.Bot, botToken);
+
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandsAsync()
+        {
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly());
+
+            _client.MessageReceived += HandleCommandAsync;
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+
+            var message =  arg as SocketUserMessage;
+ 
+            if (message is null || message.Author.IsBot)
+            {
+                return;
+            }
+
+            int argPos = 0;
+
+            if (message.HasStringPrefix(".chams ", ref argPos))
+            {
+                var context = new SocketCommandContext(_client, message);
+
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+
+                if (!result.IsSuccess)
+                {
+                    Console.WriteLine(result.ErrorReason);
+                }
+            }
+        }
